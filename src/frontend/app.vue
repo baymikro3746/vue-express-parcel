d2e9e79089fb0740458c7fc2ad056ab071b4127c
Bayram Özkulluk <32390376+baymikro3746@users.noreply.github.com>
2020-12-05 01:51:59 +0300

    Initial commit

---
+++ src/frontend/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    Vue Parcel Express
+  </div>
+</template>
+
+<script>
+  export default {
+ 
+  }
+</script>
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import Vue from 'vue/dist/vue.js'
+import App from './frontend/app'
+
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
